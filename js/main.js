2dd8923a4ccbb5470a4e1fd2e1803a17a9d85490
Moises Contreras <moises.xiaomi@gmail.com>
2023-09-28 19:55:09 -0300

    Simulador 1.1
    3. Buscador de proyecto
    4. Realizar donación

--- js/main.js
+++ js/main.js
@@ -4,21 +4,24 @@ Funciones
 1. Inicio sesión
 2. Registro de usuario
 3. Buscador de proyecto
+4. Realizar donación
 */
 
-/* 1. Inicio sesión / 2. Registro de usuario */
+/* 1. Inicio sesión / 2. Registro de usuario */  
 
- alert("Bienvenido a la página Crowdfunding de videojuegos y comics. ¡Continuemos!");
+alert("Bienvenido a la página Crowdfunding de videojuegos y comics. ¡Continuemos!");
+
+let registroConfir;
 
 function aperturaRegistro() {
-    let registroConfir = parseInt(prompt("¿Estas registrado?... Coloque 0 (zero) si no lo esta, coloque 1 (uno) si lo esta"));
+    registroConfir = parseInt(prompt("¿Estas registrado?... Coloque 0 (zero) si no lo esta, coloque 1 (uno) si lo esta"));
     return registroConfir;
 }
 
 aperturaRegistro();
 
 while (isNaN(registroConfir)){
-    alert("Por favor ingrese solo un número")
+    alert("Por favor ingrese solo un número");
     aperturaRegistro();
 }
 
@@ -29,3 +32,32 @@ if (registroConfir === 1){
 }else {
     alert("Por favor contacte con el administrador del sitio web");
 } 
+
+/* 3. Buscador de proyecto */
+
+let buscaProyecto = prompt("¿En cuál proyecto estas interesado?").toLowerCase();
+
+function siExiste(nombredelproyecto) {
+    
+    const proyectos = ["fifa 23","mario kart 8","zelda","gta", "final fantasy"];
+    
+    let existeProyecto = proyectos.includes(nombredelproyecto);
+
+    if (existeProyecto){
+        alert("El proyecto existe por favor deje una donación");
+    } else {
+        alert("El proyecto no existe por favor vuelva a intentar");
+    }
+
+    return existeProyecto;
+} 
+
+/* 4. Realizar donación */
+
+let existeProyecto = siExiste(buscaProyecto);
+
+if (existeProyecto) {
+    let donacion = parseInt(prompt("¿De cuanto va hacer su donación a este proyecto"));
+}
+
+console.log(existeProyecto);
